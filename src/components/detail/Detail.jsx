0318fc5042cbe208abd794753fab71cfe240cfed
Tarun Jhamb <tarunjhamb2705@gamil.com>
2024-06-13 01:02:44 +0530

    Chat section and details section UI design done

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,9 +1,60 @@
-import React from 'react';
-import "./detail.css";
+import React from 'react'
+import "./detail.css"
 
 const Detail = () => {
   return (
-    <div className='detail'>Detail</div>
+    <div className='detail'>
+      <div className="user">
+        <img src="public\avatar.png" alt="" />
+        <h2>Tarun Jhamb</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & Help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="public\camila.jpg" alt="" />
+                <span>photo_2024_1</span>
+              </div>
+              <img src="./download.png" alt="" className='icon' />
+            </div>
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="public\camila.jpg" alt="" />
+                <span>photo_2024_1</span>
+              </div>
+              <img src="./download.png" alt="" className='icon' />
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+    </div>
   )
 }
 
